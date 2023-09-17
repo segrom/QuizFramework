de8c9045c29242a94b0990d82c99577f03a72f6e
Segrom <segrom2001@mail.ru>
2023-09-17 18:47:01 +0300

    Added gitignore

---
+++ Assets/Scripts/Cards/StartCard.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Components;
+using Models;
+using TMPro;
+using UnityEngine;
+
+namespace Cards
+{
+    public class StartCard: BaseCard
+    {
+        [SerializeField] private GradientButton startButton;
+        [SerializeField] private TMP_Text descText;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            startButton.onClick.AddListener(OnNextInternal);
+            startButton.IsInteractive = true;
+        }
+
+        public void Setup(TestModel test)
+        {
+            descText.text = test.Description;
+        }
+    }
+}
---
+++ Assets/Scripts/ScriptableObjects/QuestionScriptableObject.cs
@@ -0,0 +1,13 @@
+﻿using Models;
+using UnityEngine;
+
+namespace ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "Question", menuName = "QuestionScriptableObject", order = 0)]
+    public class QuestionScriptableObject : ScriptableObject
+    {
+        public string imagePath;
+        public string question;
+        public QuestionOptionModel[] options;
+    }
+}
